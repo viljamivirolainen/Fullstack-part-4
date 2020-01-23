21d359877a8459bd9c245afbc4016e45da9dad2e
Matti Luukkainen <mluukkai@iki.fi>
2020-01-23 19:56:34 +0200

    part 3-2

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
+
 let notes = [
   {
     id: 1,
@@ -77,6 +91,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
