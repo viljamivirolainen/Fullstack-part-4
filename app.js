903fa0d984582d7e5524dbfe4190423dcc2e71f6
Matti Luukkainen <mluukkai@iki.fi>
2020-01-28 16:36:28 +0200

    part4-5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
