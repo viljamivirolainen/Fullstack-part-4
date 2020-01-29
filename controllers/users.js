dc29c5e68b61789ae25067cb2be12e1440a16551
Matti Luukkainen <mluukkai@iki.fi>
2020-01-29 23:01:49 +0200

    users route

--- controllers/users.js
+++ controllers/users.js
@@ -2,25 +2,26 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response, next) => {
-  try {
-    const body = request.body
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(u => u.toJSON()))
+})
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
 
-    const user = new User({
-      username: body.username,
-      name: body.name,
-      passwordHash,
-    })
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-    const savedUser = await user.save()
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
 
-    response.json(savedUser)
-  } catch (exception) {
-    next(exception)
-  }
+  const savedUser = await user.save()
+
+  response.json(savedUser)
 })
 
 module.exports = usersRouter
