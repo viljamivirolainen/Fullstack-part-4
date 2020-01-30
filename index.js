66e1479731e93e1e7ba31ab34be0fa4b2561d7df
Matti Luukkainen <mluukkai@iki.fi>
2020-01-30 21:20:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
 app.use(cors())
-
-app.use(bodyParser.json())
-
+app.use(express.json())
 app.use(express.static('build'))
 
 let notes = [
