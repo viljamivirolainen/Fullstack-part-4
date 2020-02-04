bb5bb6f28689e504d7355275da811f57dd9842b4
Matti Luukkainen <mluukkai@iki.fi>
2020-02-04 21:26:52 +0200

    Update notes.js

--- controllers/notes.js
+++ controllers/notes.js
@@ -9,18 +9,6 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes.map(note => note.toJSON()))
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -47,6 +35,21 @@ notesRouter.get('/:id', async (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 notesRouter.delete('/:id', async (request, response) => {
